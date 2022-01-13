95ab2efb248e9e60ff6d4b0c3102387f8f3f7aa6
JenerikMind <darkheart_wolf@yahoo.com>
2022-01-13 06:56:38 -0500

    Update knexfile.js

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -8,10 +8,10 @@ require('dotenv').config();
 const path = require("path");
 
 const {
-  DATABASE_URL = "postgres://nyrdxoee:t-ItHjYoaO4wWLogIQhRzrfr-OL9DivG@castor.db.elephantsql.com/nyrdxoee",
-  DATABASE_URL_DEVELOPMENT = "postgres://dcnczavs:M56-9TPPewVTESadYnhSkM1F_m_yGwAc@chunee.db.elephantsql.com/dcnczavs",
-  DATABASE_URL_TEST = "postgres://jqpprqkb:RUZM3-WhH_AgXFU_zUgCqqPVzYlqF6Du@castor.db.elephantsql.com/jqpprqkb",
-  DATABASE_URL_PREVIEW = "postgres://vdhzansf:fTFtV1s0V_4nrLqXi-he9TQU1WVQcD9C@castor.db.elephantsql.com/vdhzansf",
+  DATABASE_URL = "",
+  DATABASE_URL_DEVELOPMENT = "",
+  DATABASE_URL_TEST = "",
+  DATABASE_URL_PREVIEW = "",
   DEBUG,
 } = process.env;
 
